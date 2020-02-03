32f962979353991111b61b0264959dbdf7bd290e
MlabaE <mlabaevanne@gmail.com>
2020-02-03 15:17:42 +0200

    deliverable3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component} from 'react';
+import Todos from './todos'
+import AddTodo from './addTodo'
 
-function App() {
+class App extends Component {
+
+state = {
+  todos: [
+    {id:1, content: 'begin react tasks'},
+    {id:2, content:'dinner with bob on tuesday'}
+  ]
+}
+
+ deleteTodo = (id) => {
+   const todos = this.state.todos.filter(todo => {
+     return todo.id !== id 
+   });
+   this.setState({
+     todos
+   })
+ }
+
+ addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+ }
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-app container">
+      <h1 className="center blue-text">Todo's</h1>
+      <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+      <AddTodo addTodo={this.addTodo}/>
     </div>
   );
+  }
 }
 
 export default App;
